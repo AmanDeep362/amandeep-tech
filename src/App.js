6b5d641ca70b0a0cdd4a8d8c7f22afec9ad73140
amandeepheliverse <amandeep@heliverse.com>
2023-06-17 18:44:45 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import useCronJob from "usecronjob"
 import "./App.scss";
 import Main from "./containers/Main";
 function App() {
-  const task = () => {
-    // Perform your task here
-    console.log('Task executed!');
-  };
 
-  // Run the task every day at 12:00 PM
-  useCronJob(task, ['daily', null, '11:09']);
 
   return (
     <div>
